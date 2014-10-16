a6423b72ef2ac3af7de2067f4ca69c1708cb42a8
David Moreno García <david.mogar@gmail.com>
2014-10-16 18:10:48 +0200

    Format code

--- quizzer/deserializers/AssessmentDeserializer.php
+++ quizzer/deserializers/AssessmentDeserializer.php
@@ -5,24 +5,25 @@ require_once 'quizzer/domain/Grade.php';
 require_once 'quizzer/domain/questions/MultichoiceQuestion.php';
 require_once 'quizzer/domain/questions/TrueFalseQuestion.php';
 
-class AssessmentDeserializer {
-
+class AssessmentDeserializer
+{
     /**
      * Deserializes the JSON representation received as arguments to a map of student ids to Answer objects.
      *
      * @param $json JSON representation of the answers objects
      * @return array an array of student ids to Answer objects
      */
-    public static function deserializeAnswers($json) {
+    public static function deserializeAnswers($json)
+    {
         $data = json_decode($json);
         $answers = array();
 
         if (isset($data->items)) {
-            foreach($data->items as $student) {
+            foreach ($data->items as $student) {
                 if (isset($student->studentId) && isset($student->answers)) {
                     $studentAnswers = array();
 
-                    foreach($student->answers as $answer) {
+                    foreach ($student->answers as $answer) {
                         if (isset($answer->question) && isset($answer->value)) {
                             $studentAnswers[] = new Answer($answer->question, $answer->value);
                         }
@@ -42,12 +43,13 @@ class AssessmentDeserializer {
      * @param $json JSON representation of the grades objects
      * @return array an array of student ids to Grade objects
      */
-    public static function deserializeGrades($json) {
+    public static function deserializeGrades($json)
+    {
         $data = json_decode($json);
         $grades = array();
 
         if (isset($data->scores)) {
-            foreach($data->scores as $grade) {
+            foreach ($data->scores as $grade) {
                 if (isset($grade->studentId) && isset($grade->value)) {
                     $grades[$grade->studentId] = new Grade($grade->studentId, $grade->value);
                 }
@@ -63,12 +65,13 @@ class AssessmentDeserializer {
      * @param $json JSON representation of the questions objects
      * @return array an array of question ids to Question objects
      */
-    public static function deserializeQuestions($json) {
+    public static function deserializeQuestions($json)
+    {
         $data = json_decode($json);
         $questions = array();
 
         if (isset($data->questions)) {
-            foreach($data->questions as $question) {
+            foreach ($data->questions as $question) {
                 if (isset($question->type) && isset($question->id) && isset($question->questionText)) {
                     $newQuestion = null;
 
@@ -76,7 +79,8 @@ class AssessmentDeserializer {
                         $newQuestion = self::createMultichoiceQuestion($question);
                     } else if ($question->type == 'numerical') {
                         $newQuestion = self::createNumericalQuestion($question);
-                    } if ($question->type == 'truefalse') {
+                    }
+                    if ($question->type == 'truefalse') {
                         $newQuestion = self::createTruefalseQuestion($question);
                     }
 
@@ -90,12 +94,13 @@ class AssessmentDeserializer {
         return $questions;
     }
 
-    private static function createMultichoiceQuestion($question) {
+    private static function createMultichoiceQuestion($question)
+    {
         $multichoice = null;
 
         if (isset($question->alternatives)) {
             $multichoice = new MultichoiceQuestion($question->id, $question->questionText);
-            foreach($question->alternatives as $alternative) {
+            foreach ($question->alternatives as $alternative) {
                 if (isset($alternative->text) && isset($alternative->code) && isset($alternative->value)) {
                     $multichoice->addAlternative($alternative->code, $alternative->text, $alternative->value);
                 }
@@ -105,7 +110,8 @@ class AssessmentDeserializer {
         return $multichoice;
     }
 
-    private static function createNumericalQuestion($question) {
+    private static function createNumericalQuestion($question)
+    {
         $numerical = null;
 
         if (isset($question->correct) && isset($question->valueOK) && isset($question->valueFailed)) {
@@ -119,11 +125,13 @@ class AssessmentDeserializer {
         return $numerical;
     }
 
-    private static function createTrueFalseQuestion($question) {
+    private static function createTrueFalseQuestion($question)
+    {
         $truefalse = null;
 
         if (isset($question->correct) && isset($question->valueOK) && isset($question->valueFailed)
-                && isset($question->feedback)) {
+            && isset($question->feedback)
+        ) {
             $truefalse = new TrueFalseQuestion($question->id, $question->questionText);
             $truefalse->setCorrect($question->correct);
             $truefalse->setValueCorrect($question->valueOK);
